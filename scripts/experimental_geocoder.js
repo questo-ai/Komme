3ee6c0859c13e842c5c262dafc636b835739aba4
Arya <arya.vohra04@gmail.com>
2016-03-02 22:07:38 +0800

    Pls

--- scripts/experimental_geocoder.js
+++ scripts/experimental_geocoder.js
@@ -19,29 +19,3 @@ function initialize() {
         geocodeAddress(geocoder, map);
     });
 }
-
-
-
-//function geocodeAddress(geocoder, resultsMap) {
-//    var address = document.getElementById('address').value;
-//    geocoder.geocode({
-//        'address': address
-//    }, function (results, status) {
-//        if (status === google.maps.GeocoderStatus.OK) {
-//            resultsMap.setCenter(results[0].geometry.location);
-//            panorama = new google.maps.StreetViewPanorama(
-//                document.getElementById('street-view'), {
-//                    position: {
-//                        results[0].geometry.location
-//                    }
-//                    , pov: {
-//                        heading: 165
-//                        , pitch: 0
-//                    }
-//                    , zoom: 1
-//                });
-//        } else {
-//            alert('Geocode was not successful for the following reason: ' + status);
-//        }
-//    });
-//}
