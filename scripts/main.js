e11384ac60281d6af75209853e544b2844da5631
Arya <arya.vohra04@gmail.com>
2016-03-02 21:12:44 +0800

    stability with main.js
    made the main.js the one that actually works

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,3 @@
-var latitude;
-var longitude;
-var place = {lat: latitude, lng: longitude};
-
 function geocodeAddress(geocoder, resultsMap) {
     var address = document.getElementById('address').value;
     geocoder.geocode({
@@ -9,7 +5,7 @@ function geocodeAddress(geocoder, resultsMap) {
     }, function (results, status) {
         if (status === google.maps.GeocoderStatus.OK) {
             resultsMap.setCenter(results[0].geometry.location);
-            var place = new google.maps.Marker({
+            var marker = new google.maps.Marker({
                 map: resultsMap
                 , position: results[0].geometry.location
             });
@@ -19,17 +15,17 @@ function geocodeAddress(geocoder, resultsMap) {
     });
 }
 
-function initWindow() {
-    var panorama = new google.maps.StreetViewPanorama(
-      document.getElementById('map'), {
-        position: place,
-        pov: {
-          heading: 34,
-          pitch: 10
+function initMap() {
+    var map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 8
+        , center: {
+            lat: -34.397
+            , lng: 150.644
         }
-      });
-var geocoder = new google.maps.Geocoder();
+    });
+    var geocoder = new google.maps.Geocoder();
 
-document.getElementById('submit').addEventListener('click', function () {
-    geocodeAddress(geocoder, map);
-});
+    document.getElementById('submit').addEventListener('click', function () {
+        geocodeAddress(geocoder, map);
+    });
+}
