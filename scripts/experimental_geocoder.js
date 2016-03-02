e72dcea2271e85c90360aff9703263c0e95e5d38
Arya <arya.vohra04@gmail.com>
2016-03-02 22:01:21 +0800

    Ok

--- scripts/experimental_geocoder.js
+++ scripts/experimental_geocoder.js
@@ -1,12 +1,23 @@
 var panorama;
+
 function initialize() {
-  panorama = new google.maps.StreetViewPanorama(
-      document.getElementById('street-view'),
-      {
-        position: {lat: 37.869260, lng: -122.254811},
-        pov: {heading: 165, pitch: 0},
-        zoom: 1
-      });
+    panorama = new google.maps.StreetViewPanorama(
+        document.getElementById('street-view'), {
+            position: {
+                lat: 37.869260
+                , lng: -122.254811
+            }
+            , pov: {
+                heading: 165
+                , pitch: 0
+            }
+            , zoom: 1
+        });
+    var geocoder = new google.maps.Geocoder();
+
+    document.getElementById('submit').addEventListener('click', function () {
+        geocodeAddress(geocoder, map);
+    });
 }
 
 
