8ebf61f17f77571f15fd99b1da13a0f96c023136
Arya <arya.vohra04@gmail.com>
2016-03-02 18:47:04 +0800

    Stuff
    cbb

--- window.js
+++ window.js
@@ -1,9 +1,10 @@
+@import zone.js
 function initMap() {
     var map = new google.maps.Map(document.getElementById('map'), {
         zoom: 8
         , center: {
-            lat: -34.397
-            , lng: 150.644
+            lat: latitude
+            , lng: longitude
         }
     });
     var geocoder = new google.maps.Geocoder();
@@ -13,19 +14,12 @@ function initMap() {
     });
 }
 
-function geocodeAddress(geocoder, resultsMap) {
-    var address = document.getElementById('address').value;
-    geocoder.geocode({
-        'address': address
-    }, function (results, status) {
-        if (status === google.maps.GeocoderStatus.OK) {
-            resultsMap.setCenter(results[0].geometry.location);
-            var marker = new google.maps.Marker({
-                map: resultsMap
-                , position: results[0].geometry.location
-            });
-        } else {
-            alert('Geocode was not successful for the following reason: ' + status);
+var panorama = new google.maps.StreetViewPanorama(
+    document.getElementById('pano'), {
+        position: place
+        , pov: {
+            heading: 34
+            , pitch: 10
         }
     });
-}
+map.setStreetView(panorama);
