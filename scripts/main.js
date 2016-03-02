09726c49cf4beb4a8ee11a8aab8995c261cd50bf
Arya <arya.vohra04@gmail.com>
2016-03-02 19:50:51 +0800

    Main.js
    Adding the street view stuff, and creating the new calls

--- scripts/main.js
+++ scripts/main.js
@@ -9,7 +9,7 @@ function geocodeAddress(geocoder, resultsMap) {
     }, function (results, status) {
         if (status === google.maps.GeocoderStatus.OK) {
             resultsMap.setCenter(results[0].geometry.location);
-            var marker = new google.maps.Marker({
+            var place = new google.maps.Marker({
                 map: resultsMap
                 , position: results[0].geometry.location
             });
@@ -19,17 +19,17 @@ function geocodeAddress(geocoder, resultsMap) {
     });
 }
 
-function initMap() {
-    var map = new google.maps.Map(document.getElementById('map'), {
-        zoom: 8
-        , center: {
-            lat: latitude
-            , lng: longitude
+function initWindow() {
+    var panorama = new google.maps.StreetViewPanorama(
+      document.getElementById('pano'), {
+        position: place,
+        pov: {
+          heading: 34,
+          pitch: 10
         }
-    });
-    var geocoder = new google.maps.Geocoder();
+      });
+var geocoder = new google.maps.Geocoder();
 
-    document.getElementById('submit').addEventListener('click', function () {
-        geocodeAddress(geocoder, map);
-    });
-}
+document.getElementById('submit').addEventListener('click', function () {
+    geocodeAddress(geocoder, map);
+});
