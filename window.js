fa981f7b3425a0317f0c42a36a2d470953ab0a09
tkato0909 <tkato.main@gmail.com>
2016-03-02 08:15:26 +0800

    Added geocode

--- window.js
+++ window.js
@@ -1,16 +1,35 @@
+<<<<<<< Updated upstream
 function initialize() {
         var fenway = {lat: 'latitude', lng: 'longitude'};
         var map = new google.maps.Map(document.getElementById('map'), {
           center: fenway,
           zoom: 14
+=======
+var geocoder;
+  var map;
+  function initialize() {
+    geocoder = new google.maps.Geocoder();
+    var latlng = new google.maps.LatLng(1.352083, 103.819836);
+    var mapOptions = {
+      zoom: 8,
+      center: latlng
+    }
+    map = new google.maps.Map(document.getElementById("map"), mapOptions);
+  }
+
+  function codeAddress() {
+    var address = document.getElementById("address").value;
+    geocoder.geocode( { 'address': address}, function(results, status) {
+      if (status == google.maps.GeocoderStatus.OK) {
+        map.setCenter(results[0].geometry.location);
+        var marker = new google.maps.Marker({
+            map: map,
+            position: results[0].geometry.location
+>>>>>>> Stashed changes
         });
-        var panorama = new google.maps.StreetViewPanorama(
-            document.getElementById('pano'), {
-              position: fenway,
-              pov: {
-                heading: 34,
-                pitch: 10
-              }
-            });
-        map.setStreetView(panorama);
+      } else {
+        alert("Geocode was not successful for the following reason: " + status);
       }
+    });
+  }
+  
